9928c9d8860b008b29745f4c1c97a29679213e20
Natalya <nanona@example.com>
2022-11-22 14:49:46 +0700

    Added Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+
+int number = ReadInt("Введите число:");
+
+int count = 1;
+
+while (count <= number)
+{
+    Console.Write(count * count * count + " ");
+    count++;
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
